885b2f8aaa9e95a57dfca86440480d1018adc71e
Nadav Ivgi <nadav@shesek.info>
2018-02-15 09:15:15 +0200

    add nib to stylus, compress on production, refactor assets

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-import fs from 'fs'
-import path from 'path'
-import LightningClient from 'lightning-client'
-
-const rel = p => path.join(__dirname, p)
-
-const lnPath = process.env.LN_PATH || path.join(require('os').homedir(), '.lightning')
+const lnPath = process.env.LN_PATH || require('path').join(require('os').homedir(), '.lightning')
 
 const app = require('express')()
-    , ln  = LightningClient(lnPath)
+    , ln  = require('lightning-client')(lnPath)
 
 app.set('port', process.env.PORT || 9117)
 app.set('host', process.env.HOST || 'localhost')
@@ -18,13 +12,10 @@ app.set('trust proxy', process.env.PROXIED || 'loopback')
 app.use(require('cookie-parser')())
 app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: true }))
-
 app.use(require('morgan')('dev'))
 app.use(require('csurf')({ cookie: true }))
 
-app.get('/app.js', require('browserify-middleware')(rel('client/app.js')))
-app.use('/assets', require('stylus').middleware({ src: rel('www'), serve: true }))
-app.use('/assets', require('express').static(rel('www')))
+require('./assets')(app)
 
 app.get('/', (req, res) => res.render('index.pug', { req }))
 
@@ -39,6 +30,10 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.type && err || err.stack || err)
 })
 
-const sslOpt = { key: fs.readFileSync(rel('key.pem')), cert: fs.readFileSync(rel('cert.pem')) }
+
+import fs from 'fs'
+
+const sslOpt = { key: fs.readFileSync('key.pem'), cert: fs.readFileSync('cert.pem') }
+
 require('https').createServer(sslOpt, app).listen(app.settings.port, app.settings.host, _ =>
   console.log(`HTTP server running on ${ app.settings.host }:${ app.settings.port }`))
---
+++ assets.js
@@ -0,0 +1,19 @@
+import { join } from 'path'
+
+import stylus from 'stylus'
+import express from 'express'
+import browserify from 'browserify-middleware'
+
+module.exports = app => {
+
+  const compileStyl = (str, filename) => stylus(str)
+    .set('filename', filename)
+    .set('include css', true)
+    .set('compress', app.settings.env == 'production')
+    .use(require('nib')())
+    .import('nib')
+
+  app.get('/app.js', browserify(join(__dirname, 'client/app.js')))
+  app.use('/assets', stylus.middleware({ src: join(__dirname, 'www'), serve: true, compile: compileStyl }))
+  app.use('/assets', express.static(join(__dirname, 'www')))
+}
