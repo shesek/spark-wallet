6f5762cd2c3e94d6b39eb2d842ac531788c0bc31
Nadav Ivgi <nadav@shesek.info>
2018-04-10 02:46:54 +0300

    improve cordova build script

--- build-cordova.sh
+++ build-cordova.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 DEST=cordova-app/www
+
 rm -r $DEST/*
 
-export TARGET_CORDOVA=1
 cp -rL www $DEST/assets
+find $DEST/assets/bootswatch -type f ! -name '*.min.css' -exec rm {} +
+
+export BUILD_TARGET='cordova'
+
 pug index.pug -o $DEST
-browserify client/app.js > $DEST/app.js
+browserify client/app.js | uglifyjs -c warnings=false -m > $DEST/app.js
 stylus -u nib styl/style.styl $DEST/assets
 
