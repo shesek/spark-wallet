0f88316cb6535a993487b8ce9aff16df38f3b16f
Nadav Ivgi <nadav@shesek.info>
2018-04-10 04:49:27 +0300

    expand comment

--- client/driver/conf.js
+++ client/driver/conf.js
@@ -1,7 +1,8 @@
 import dropRepeats from 'xstream/extra/dropRepeats'
 import { dbg } from '../util'
 
-// @XXX the code below mixes rxjs and xstream observable together, should be cleaned up
+// @XXX the code below mixes rxjs and xstream observables together, normalize them?
+// (cyclejs provides conf$ as an xstream, while the storage driver returns rxjs streams)
 
 module.exports = storage => conf$ =>
   storage(conf$.map(JSON.stringify).compose(dropRepeats()).map(conf => ({ key: 'conf', value: conf })))
