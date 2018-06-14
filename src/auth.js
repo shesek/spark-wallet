00946105489589bf4a9af8efede7ec1a6b9c2864
Nadav Ivgi <nadav@shesek.info>
2018-06-14 07:05:21 +0300

    manifest.json: exempt from http basic auth, use query string instead

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,8 @@
 import basicAuth from 'basic-auth'
 import nanoid from 'nanoid'
 import nanogen from 'nanoid/generate'
+import base64u from 'base64-url'
+import {createHmac} from 'crypto'
 
 module.exports = (app, login) => {
   let username, password
@@ -15,10 +17,15 @@ module.exports = (app, login) => {
   }
 
   app.settings.encAuth = [ username, password ].map(encodeURIComponent).join(':')
+  app.settings.manifestKey = base64u.escape(createHmac('sha256', app.settings.encAuth).update('manifest-key').digest('base64'))
 
   return (req, res, next) => {
-    const cred = basicAuth(req)
+    // The manifest.json file has to be accessible without basic auth headers,
+    // allow authenticating via query string instead
+    if (req.method === 'GET' && req.path === '/manifest.json' && req.query.key === app.settings.manifestKey)
+      return next()
 
+    const cred = basicAuth(req)
     if (cred && cred.name === username && cred.pass === password)
       return next()
 
