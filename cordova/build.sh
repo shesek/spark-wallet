6378b57f7b6c379c9e004432234c0b95b9c59df1
Nadav Ivgi <nadav@shesek.info>
2018-06-25 17:58:10 +0300

    Use Cordova hooks instead of a wrapper script

--- cordova/build.sh
+++ cordova/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -xeo pipefail
-export PATH=../node_modules/.bin:$PATH
 
 mkdir -p www
 rm -rf www/*
 
 export BUILD_TARGET=cordova
 (cd ../client && DEST=../cordova/www npm run dist)
-
-cordova prepare && cordova build
