e89f5b4e5ebc07e681f543bc187203ee0b60ff2b
Nadav Ivgi <nadav@shesek.info>
2018-06-26 09:27:30 +0300

    Rename debug labels

--- client/src/app.js
+++ client/src/app.js
@@ -33,8 +33,8 @@ const main = ({ DOM, HTTP, SSE, route, conf$, scan$, urihandler$ }) => {
       , notif$  = view.notif({ state$, ...resps })
       , orient$ = view.orient(actions.page$)
 
-  dbg(actions, 'spark:intent:actions')
-  dbg(resps, 'spark:intent:rpc')
+  dbg(actions, 'spark:intent')
+  dbg(resps, 'spark:rpc')
   dbg({ state$ }, 'spark:model')
   dbg({ rpc$, vdom$, navto$, notif$, orient$ }, 'spark:sinks')
 
