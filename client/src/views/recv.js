6976a529147af753e9973d2aabe5a3ba58e0ed67
Nadav Ivgi <nadav@shesek.info>
2018-06-27 09:58:50 +0300

    Indent (content remained unchanged)

--- client/src/views/recv.js
+++ client/src/views/recv.js
@@ -15,21 +15,22 @@ const recv = ({ amtData }) =>
   , a('.btn.btn-lg.btn-secondary.mb-2', { attrs: { href: '#/' } }, 'Cancel')
   ])
 
-const invoice = inv => qrinv(inv).then(qr => ({ unitf, conf: { expert } }) => div('.waiting-payment', [
-  div('.row', [
-    div('.col-sm-6.text-center', [
-      h2('Waiting for payment')
-    , inv.msatoshi !== 'any' ? h3('.toggle-unit', unitf(inv.msatoshi)) : ''
-    , small('.d-none.d-sm-block.text-muted.break-all.mt-3', inv.bolt11)
-    ])
-  , div('.col-sm-6.text-center.text-sm-right', [
-      img('.qr', { attrs: { src: qr } })
-    , small('.d-block.d-sm-none.text-center.text-muted.break-all.mt-3', inv.bolt11)
-    ])
+const invoice = inv => qrinv(inv).then(qr => ({ unitf, conf: { expert } }) =>
+  div('.waiting-payment', [
+    div('.row', [
+      div('.col-sm-6.text-center', [
+        h2('Waiting for payment')
+      , inv.msatoshi !== 'any' ? h3('.toggle-unit', unitf(inv.msatoshi)) : ''
+      , small('.d-none.d-sm-block.text-muted.break-all.mt-3', inv.bolt11)
+      ])
+    , div('.col-sm-6.text-center.text-sm-right', [
+        img('.qr', { attrs: { src: qr } })
+      , small('.d-block.d-sm-none.text-center.text-muted.break-all.mt-3', inv.bolt11)
+      ])
 
-  ])
-, expert ? yaml(omitKey('bolt11', inv)) : ''
-]))
+    ])
+  , expert ? yaml(omitKey('bolt11', inv)) : ''
+  ]))
 
 const omitKey = (k, { [k]: _, ...rest }) => rest
 
