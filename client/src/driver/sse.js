d1a7910382fd681db0f19d40823c06753f48fc5b
Nadav Ivgi <nadav@shesek.info>
2018-06-28 11:08:33 +0300

    Revert 74824779554faac6aed200f1a63724aaf4b55a54
    This was apparently caused by using the eventsource shim, using the
    browser native EventSource instead doesn't cause the loading indicator
    issue.

--- client/src/driver/sse.js
+++ client/src/driver/sse.js
@@ -1,10 +1,6 @@
 import { Observable as O } from '../rxjs'
 
 module.exports = url => {
-  // Only start SSE after the page loads, so that we don't get an infinite loading indicator spinner
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=95227
-  // https://stackoverflow.com/questions/9932462/how-to-remove-safaris-loading-wheel-when-using-server-sent-events
-  const es$ = O.fromEvent(window, 'load').first().map(_ => new EventSource(url)).shareReplay(1)
-
-  return _ => (ev='message') => es$.switchMap(es => O.fromEvent(es, ev).map(r => r.data).map(JSON.parse))
+  const es = new EventSource(url)
+  return _ => (ev='message') => O.fromEvent(es, ev).map(r => r.data).map(JSON.parse)
 }
