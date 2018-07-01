782469c48d9da451b9105f5a1ca22eacc6ac0e3f
Nadav Ivgi <nadav@shesek.info>
2018-07-01 10:01:38 +0300

    Bugfix: Properly set main DataDirectory for Tor

--- src/transport/onion.js
+++ src/transport/onion.js
@@ -3,16 +3,17 @@ import path from 'path'
 import http from 'http'
 import fs   from 'fs'
 
-module.exports = (app, dir=path.resolve('spark-tor')) =>
+module.exports = (app, dir=path.resolve('spark-tor'), hs_dir=path.join(dir, 'hidden_service')) =>
   new Promise(resolve => {
     // Start HTTP server (non TLS) on a random port
     const httpSrv = http.createServer(app).listen(0, '127.0.0.1', _ =>
       resolve(httpSrv.address().port))
   }).then(httpPort => new Promise((resolve, reject) =>
     // Setup an hidden service over the HTTP server
-    hsv3([ { dataDirectory: dir, virtualPort: 80, localMapping: '127.0.0.1:' + httpPort  }  ])
+    hsv3([ { dataDirectory: hs_dir, virtualPort: 80, localMapping: '127.0.0.1:' + httpPort  } ]
+       , { DataDirectory: dir })
       .on('error', reject)
-      .on('ready', _ => resolve(getHost(dir)))
+      .on('ready', _ => resolve(getHost(hs_dir)))
   ))
 
 const getHost = dir => fs.readFileSync(path.join(dir, 'hostname')).toString().trim()
