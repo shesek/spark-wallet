a7a43f12c3ce8a68e99a87dbcff11e433b2008ca
Nadav Ivgi <nadav@shesek.info>
2018-07-10 03:54:19 +0300

    Electron: setup electorn-debug

--- electron/main.js
+++ electron/main.js
@@ -1,13 +1,13 @@
 const {app, BrowserWindow} = require('electron')
 
+require('electron-debug')({ showDevTools: false })
+
 let mainWindow
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 630, height: 900})
 
   mainWindow.loadFile('www/index.html')
-
-  //mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () { mainWindow = null  })
 }
