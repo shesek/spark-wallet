15f277d0ef50fc0e0be1f9764bac0ac45d51ad99
Nadav Ivgi <nadav@shesek.info>
2018-07-10 04:07:08 +0300

    Electron: tidy up main

--- electron/main.js
+++ electron/main.js
@@ -6,23 +6,10 @@ let mainWindow
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 630, height: 900})
-
   mainWindow.loadFile('www/index.html')
-
-  mainWindow.on('closed', function () { mainWindow = null  })
+  mainWindow.on('closed', _ => mainWindow = null)
 }
 
 app.on('ready', createWindow)
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
+app.on('window-all-closed', _ => process.platform === 'darwin' || app.quit())
+app.on('activate', _ => mainWindow || createWindow())
