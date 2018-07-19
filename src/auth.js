11ebc32ee229f7cb794f4dfcf7ac92c0c49eebce
Nadav Ivgi <nadav@shesek.info>
2018-07-19 12:02:03 +0300

    Longer default password

--- src/auth.js
+++ src/auth.js
@@ -14,7 +14,7 @@ module.exports = (app, login, _accessKey) => {
 
   if (!login) {
     username = nanogen('abcdefghijklmnopqrstuvwxyz', 5)
-    password = nanoid(10)
+    password = nanoid(15)
     console.log(`No LOGIN or --login specified, picked username "${username}" with password "${password}"`)
   } else {
     [ username, password ] = login.split(':', 2)
