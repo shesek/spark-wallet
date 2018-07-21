bc5cc117f1715b268147cddcfee0e91888093576
Nadav Ivgi <nadav@shesek.info>
2018-07-21 16:47:46 +0300

    Electron: share IPC stream

--- client/src/driver/electron-ipc.js
+++ client/src/driver/electron-ipc.js
@@ -4,5 +4,5 @@ const { ipcRenderer } = window
 
 module.exports = cmd$ => (
   O.from(cmd$).subscribe(cmd => ipcRenderer.send(...cmd))
-, topic => O.fromEvent(ipcRenderer, topic, (e, arg) => arg)
+, topic => O.fromEvent(ipcRenderer, topic, (e, arg) => arg).share()
 )
