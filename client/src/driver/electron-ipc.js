1ca1c89e6335a44cab3d3a3e246565380cc00fc7
Nadav Ivgi <nadav@shesek.info>
2018-07-24 09:12:32 +0300

    Electron: Log IPC serverErrors to browser console

--- client/src/driver/electron-ipc.js
+++ client/src/driver/electron-ipc.js
@@ -2,6 +2,8 @@ import { Observable as O } from '../rxjs'
 
 const { ipcRenderer } = window
 
+ipcRenderer.on('serverError', err => console.error('Spark server error:', err))
+
 module.exports = cmd$ => (
   O.from(cmd$).subscribe(cmd => ipcRenderer.send(...cmd))
 , topic => O.fromEvent(ipcRenderer, topic, (e, arg) => arg).share()
