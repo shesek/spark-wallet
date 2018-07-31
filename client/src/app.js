90c113841abea06697eacc2c5bc26416e620c352
Nadav Ivgi <nadav@shesek.info>
2018-07-31 21:48:25 +0300

    Check service worker is available before attempting to register it

--- client/src/app.js
+++ client/src/app.js
@@ -91,6 +91,6 @@ run(main, {
 : {})
 })
 
-if (process.env.BUILD_TARGET == 'web')
+if (process.env.BUILD_TARGET == 'web' && navigator.serviceWorker)
   window.addEventListener('load', _ => navigator.serviceWorker.register('worker.js'))
 
