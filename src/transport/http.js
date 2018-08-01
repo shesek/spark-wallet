540e81679213d1f98bcf0d992a18866b87e45159
Nadav Ivgi <nadav@shesek.info>
2018-08-01 03:47:59 +0300

    Derive server URL from provided --host (and not the detected listening address)

--- src/transport/http.js
+++ src/transport/http.js
@@ -2,5 +2,5 @@ import http from 'http'
 
 module.exports = app => new Promise(resolve => {
   const httpSrv = http.createServer(app).listen(app.settings.port, app.settings.host, _ =>
-    resolve(`http://${httpSrv.address().address}:${httpSrv.address().port}`))
+    resolve(`http://${app.settings.host}:${httpSrv.address().port}`))
 })
