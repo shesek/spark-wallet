2b2be3bb4fb0416d34c63504178d343662222dd7
Nadav Ivgi <nadav@shesek.info>
2018-09-30 18:24:19 +0300

    Trim BOLT11 URIs before parsing them. Closes #28.

--- client/src/util.js
+++ client/src/util.js
@@ -13,7 +13,7 @@ const
       : ''
 
 , parseUri = uri => {
-    const m = uri.match(reUri)
+    const m = uri.trim().match(reUri)
     return m && (m[1] || m[2] || m[3])
   }
 , reUri = /^lightning:([a-z0-9]+)|^bitcoin:.*[?&]lightning=([a-z0-9]+)|^(ln[a-z0-9]+)$/i
