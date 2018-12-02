d3637bc3912db68b62bd317c827ef0924f43eb5d
Nadav Ivgi <nadav@shesek.info>
2018-12-02 03:48:56 +0200

    Use ES6 style export

--- client/src/util.js
+++ client/src/util.js
@@ -4,53 +4,48 @@ import numbro from 'numbro'
 import { Big as big }      from 'big.js'
 import { Observable as O } from './rxjs'
 
-const
+export const formatAmt = (amt, rate, digits, comma=true) =>
+  (amt != null && ''+amt && rate)
+    ? numbro(big(amt).times(rate).toFixed(digits))
+        .format({ thousandSeparated: comma, mantissa: digits, trimMantissa: true, optionalMantissa: true })
+    : ''
 
-  formatAmt = (amt, rate, digits, comma=true) =>
-    (amt != null && ''+amt && rate)
-      ? numbro(big(amt).times(rate).toFixed(digits))
-          .format({ thousandSeparated: comma, mantissa: digits, trimMantissa: true, optionalMantissa: true })
-      : ''
+const reUri = /^lightning:([a-z0-9]+)|^bitcoin:.*[?&]lightning=([a-z0-9]+)|^(ln[a-z0-9]+)$/i
 
-, parseUri = uri => {
-    const m = uri.trim().match(reUri)
-    return m && (m[1] || m[2] || m[3]).toLowerCase()
-  }
-, reUri = /^lightning:([a-z0-9]+)|^bitcoin:.*[?&]lightning=([a-z0-9]+)|^(ln[a-z0-9]+)$/i
+export const parseUri = uri => {
+  const m = uri.trim().match(reUri)
+  return m && (m[1] || m[2] || m[3]).toLowerCase()
+}
 
 // returns the expected invoice amount when its <0.5% different from the actual amount paid,
 // or the actual amount paid otherwise. this is done to make the UX less confusing when the
 // sender uses overpayment randomization (https://github.com/ElementsProject/lightning/issues/1089)
-, recvAmt = ({ msatoshi: expected, msatoshi_received: actual }) =>
-    (expected && (actual-expected)/expected<0.005) ? expected : actual
+export const recvAmt = ({ msatoshi: expected, msatoshi_received: actual }) =>
+  (expected && (actual-expected)/expected<0.005) ? expected : actual
 
-, combine = obj => {
-    const keys = Object.keys(obj).map(k => k.replace(/\$$/, ''))
-    return O.combineLatest(...Object.values(obj), (...xs) =>
-      xs.reduce((o, x, i) => (o[keys[i]] = x, o), {}))
-  }
+export const combine = obj => {
+  const keys = Object.keys(obj).map(k => k.replace(/\$$/, ''))
+  return O.combineLatest(...Object.values(obj), (...xs) =>
+    xs.reduce((o, x, i) => (o[keys[i]] = x, o), {}))
+}
 
-, toObs = x => (x.subscribe || x.then) ? O.from(x) : O.of(x)
+export const toObs = x => (x.subscribe || x.then) ? O.from(x) : O.of(x)
 
-, dropErrors = r$$ => r$$.flatMap(r$ => r$.catch(_ => O.empty()))
+export const dropErrors = r$$ => r$$.flatMap(r$ => r$.catch(_ => O.empty()))
 
-, extractErrors = r$$ =>
-    r$$.flatMap(r$ => r$.flatMap(_ => O.empty()).catch(err => O.of(err)))
-      .map(e => e.response && (e.response.body && e.response.body.message || e.response.body) || e)
+export const extractErrors = r$$ =>
+  r$$.flatMap(r$ => r$.flatMap(_ => O.empty()).catch(err => O.of(err)))
+    .map(e => e.response && (e.response.body && e.response.body.message || e.response.body) || e)
 
-, formatError = err => (err.message && err.message.startsWith('Request has been terminated'))
-    ? new Error('Connection to server lost.')
-    : err
+export const formatError = err => (err.message && err.message.startsWith('Request has been terminated'))
+  ? new Error('Connection to server lost.')
+  : err
 
-, connErrors = [ 'Error: Connection to server lost.', 'Error: Unauthorized', 'Error: Not Found', 'Error: Bad Gateway', 'Error: Service Unavailable' ]
-, isConnError = err => connErrors.includes(err.toString())
+const connErrors = [ 'Error: Connection to server lost.', 'Error: Unauthorized', 'Error: Not Found', 'Error: Bad Gateway', 'Error: Service Unavailable' ]
+export const isConnError = err => connErrors.includes(err.toString())
 
-, dbg = (obj, label='stream', dbg=debug(label)) =>
-    Object.keys(obj).forEach(k => obj[k] && obj[k].subscribe(
-      x   => dbg(`${k} ->`, x)
-    , err => dbg(`${k} \x1b[91mError:\x1b[0m`, err.stack || err)
-    , _   => dbg(`${k} completed`)))
-
-module.exports = { combine, toObs, dbg
-                 , dropErrors, extractErrors, formatError, isConnError
-                 , formatAmt, parseUri, recvAmt }
+export const dbg = (obj, label='stream', dbg=debug(label)) =>
+  Object.keys(obj).forEach(k => obj[k] && obj[k].subscribe(
+    x   => dbg(`${k} ->`, x)
+  , err => dbg(`${k} \x1b[91mError:\x1b[0m`, err.stack || err)
+  , _   => dbg(`${k} completed`)))
--- client/src/views/util.js
+++ client/src/views/util.js
@@ -6,28 +6,28 @@ import { isConnError } from '../util'
 
 const isOnion = global.location && /\.onion$/.test(location.hostname)
 
-const yaml = data => pre('.mt-3.text-left.text-muted', typeof data == 'string' ? data : YAML.safeDump(data))
+export const yaml = data => pre('.mt-3.text-left.text-muted', typeof data == 'string' ? data : YAML.safeDump(data))
 
 // use server-generated QRs when browsed over .onion
 // (the canvas fingerprint protection in Tor Browser breaks client-side generation)
-const qruri = process.env.BUILD_TARGET == 'web' && isOnion
+export const qruri = process.env.BUILD_TARGET == 'web' && isOnion
   ? data => Promise.resolve(`qr/${ encodeURIComponent(data) }`)
   : data => qrcode.toDataURL(data)
 
-const qrinv = inv => qruri(`lightning:${ inv.bolt11  }`.toUpperCase())
+export const qrinv = inv => qruri(`lightning:${ inv.bolt11  }`.toUpperCase())
 
 // Avoid displaying our default description (of "⚡")
-const showDesc = o => o.description && o.description !== '⚡'
+export const showDesc = o => o.description && o.description !== '⚡'
 
-const ago = ts => vagueTime.get({ to: Math.min(ts*1000, Date.now()) })
+export const ago = ts => vagueTime.get({ to: Math.min(ts*1000, Date.now()) })
 
-const formGroup = (labelText, control, help) => div('.form-group', [
+export const formGroup = (labelText, control, help) => div('.form-group', [
   label('.col-form-label.col-form-label-lg', labelText)
 , control
 , help ? small('.form-text.text-muted', help) : ''
 ])
 
-const amountField = ({ msatoshi, amount, step, unit }, msatField, required) =>
+export const amountField = ({ msatoshi, amount, step, unit }, msatField, required) =>
   div('.input-group', [
     input({ attrs: { type: 'hidden', name: msatField }, props: { value: msatoshi } })
   , input('.form-control.form-control-lg'
@@ -36,7 +36,7 @@ const amountField = ({ msatoshi, amount, step, unit }, msatField, required) =>
   , div('.input-group-append.toggle-unit', span('.input-group-text', unit))
   ])
 
-const alertBox = ([ kind, text ], dismissable) =>
+export const alertBox = ([ kind, text ], dismissable) =>
   div(`.alert${dismissable?'.alert-dismissable':''}.alert-${kind}`, [
     dismissable ? button('.close', { attrs: { type: 'button' }, dataset: { dismiss: 'alert' } }, '×') : ''
 
@@ -46,6 +46,3 @@ const alertBox = ([ kind, text ], dismissable) =>
     ? a('.alert-link', { attrs: { href: 'settings.html', rel: 'external' } }, 'Try configuring a different server?')
     : ''
   ])
-
-
-module.exports = { yaml, qruri, qrinv, showDesc, ago, formGroup, amountField, alertBox }
