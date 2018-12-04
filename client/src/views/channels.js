46df27772a29061756a7261e55097d14d7480d17
Nadav Ivgi <nadav@shesek.info>
2018-12-04 18:41:37 +0200

    Display channel age
    Shown in number of blocks based on the block height available
    in the short channel ID, as well as a fuzzy time duration estimation

--- client/src/views/channels.js
+++ client/src/views/channels.js
@@ -1,5 +1,7 @@
 import { div, h2, ul, li, span, pre, strong, small, em, button, header, a, form, input, p } from '@cycle/dom'
-import { yaml, omitKey, formGroup, amountField, getChannels } from './util'
+import { yaml, omitKey, formGroup, amountField, getChannels, ago } from './util'
+
+const blockInterval = process.env.BLOCK_INTERVAL || 600
 
 const stateGroups = {
   active: [ 'CHANNELD_NORMAL' ]
@@ -23,8 +25,10 @@ const chanSorting = ({ peer, chan }) =>
 : stateGroups.closed.includes(chan.state) ? 1
 : 0
 
-export const channels = ({ channels, unitf, chanActive, conf: { expert } }) => {
+export const channels = ({ channels, chanActive, unitf, info, conf: { expert } }) => {
   if (!channels) return '';
+
+  const blockheight = info && info.blockheight
 
   channels = channels.slice()
   channels.sort(chanSorter)
@@ -35,7 +39,7 @@ export const channels = ({ channels, unitf, chanActive, conf: { expert } }) => {
     //, ' ', a('.btn.btn-sm.btn-primary.float-right.mr-1', { attrs: { href: '#/channels/new' } }, 'New')
     ])
   , channels.length
-    ? ul('.list-group.channels', channels.map(channelRenderer({ unitf, expert, chanActive })))
+    ? ul('.list-group.channels', channels.map(channelRenderer({ unitf, expert, chanActive, blockheight })))
     : p('You have no channels.')
   , div('.text-center', a('.btn.btn-primary.mt-3', { attrs: { href: '#/channels/new' } }, 'Open channel'))
   ])
@@ -52,7 +56,7 @@ export const newChannel = ({ amtData, conf: { expert } }) =>
   , a('.btn.btn-lg.btn-secondary.mb-2', { attrs: { href: '#/channels' } }, 'Cancel')
   ])
 
-const channelRenderer = ({ chanActive, unitf, expert }) => ({ chan, peer }) => {
+const channelRenderer = ({ chanActive, unitf, expert, blockheight }) => ({ chan, peer }) => {
 
   const bar = (label, color, msatoshi, amtText=unitf(msatoshi)) =>
     div(`.progress-bar.bg-${color}`, {
@@ -62,9 +66,14 @@ const channelRenderer = ({ chanActive, unitf, expert }) => ({ chan, peer }) => {
 
   const stateGroup = getGroup(chan.state)
       , stateLabel = !peer.connected && stateGroup == 'active' ? 'offline' : stateGroup
+      , isClosed   = [ 'closing', 'closed' ].includes(stateGroup)
       , theirBal   = chan.msatoshi_total - chan.msatoshi_to_us
       , receivable = theirBal - (chan.their_channel_reserve_satoshis*1000)
-      , isClosed   = [ 'closing', 'closed' ].includes(stateGroup)
+
+  const channelHeight = chan.short_channel_id && +chan.short_channel_id.split(/[:x]/)[0]
+      , channelAge    = channelHeight && blockheight && (blockheight - channelHeight)
+      , channelAgeFuz = channelAge && ago(Date.now()/1000 - channelAge*blockInterval).replace(/ ago$/,'')
+
 
   const visible = chanActive == chan.channel_id
       , classes = { active: visible, 'list-group-item-action': !visible
@@ -96,6 +105,7 @@ const channelRenderer = ({ chanActive, unitf, expert }) => ({ chan, peer }) => {
     , isClosed ? '' : li([ strong('Spendable:'), ' ', unitf(chan.spendable_msatoshi) ])
     , isClosed ? '' : li([ strong('Receivable:'), ' ', unitf(receivable) ])
 
+    , channelAge ? li([ strong('Age:'), ' ', `${channelAge} blocks (about ${channelAgeFuz})` ]) : ''
     , li([ strong('Peer:'), ' ', small('.break-all', peer.id), ' ', em(`(${peer.connected ? 'connected' : 'disconnected'})`) ])
     , !expert ? '' : li([ strong('Funding TXID:'), ' ', small('.break-all', chan.funding_txid) ])
     , !expert ? '' : li('.status-text', chan.status.join('\n'))
