4a2f6ebbe553a47d62e4792d6cd31692769bb48d
nicolas.dorier <nicolas.dorier@gmail.com>
2019-06-10 17:44:41 +0900

    Remove manifest tagging

--- scripts/release.sh
+++ scripts/release.sh
@@ -73,13 +73,11 @@ if [[ -z "$SKIP_DOCKER" ]]; then
 
   docker tag $docker_name:$version-amd64 $docker_name:$version
   docker tag $docker_name:$version-amd64 $docker_name:latest
-  docker tag $docker_name:$version-standalone $docker_name:standalone
+  # docker tag $docker_name:$version-standalone $docker_name:standalone
   # we shouldn't push docker this early in the script, but the docker image hash is not available until we do
   # and is needed for the SHA256SUMS file. https://groups.google.com/forum/#!topic/docker-user/PvAcxDrvP30
   # https://github.com/moby/moby/issues/16482 https://github.com/docker/distribution/issues/1662
   docker push $docker_name:$version
-  docker push $docker_name:$version:latest
-  docker push $docker_name:$version:standalone
 fi
 
 # Make SHA256SUMS & sign it
