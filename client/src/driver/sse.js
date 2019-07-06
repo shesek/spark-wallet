4d2e4d61f8d48904d9a26c2a7ba266c9f89ad799
Nadav Ivgi <nadav@shesek.info>
2019-07-06 20:53:54 +0300

    Filter empty SSE events

--- client/src/driver/sse.js
+++ client/src/driver/sse.js
@@ -5,5 +5,5 @@ module.exports = serverInfo => {
   const srcUrl = url.resolve(serverInfo.serverUrl, `stream?access-key=${serverInfo.accessKey}`)
 
   const es = new EventSource(srcUrl)
-  return _ => (ev='message') => O.fromEvent(es, ev).map(r => r.data).map(JSON.parse).share()
+  return _ => (ev='message') => O.fromEvent(es, ev).map(r => r.data).filter(Boolean).map(JSON.parse).share()
 }
