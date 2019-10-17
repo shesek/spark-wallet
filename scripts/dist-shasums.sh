f7e2f08901bcfc252bc91550c337e48662d86d72
Nadav Ivgi <nadav@shesek.info>
2019-10-17 06:28:39 +0300

    Add the arm64v8 docker image to SHASUMS

--- scripts/dist-shasums.sh
+++ scripts/dist-shasums.sh
@@ -14,7 +14,7 @@ sha256sum spark-wallet-*-npm.tgz \
 if command -v docker > /dev/null; then
   version=`node -p 'require("./package.json").version'`
 
-  for name in "$version-amd64" "$version-standalone-amd64" "$version-standalone-arm32v7"
+  for name in "$version-amd64" "$version-standalone-amd64" "$version-standalone-arm32v7" "$version-standalone-arm64v8"
   do
     dockerhash=`docker inspect --format='{{index .RepoDigests 0}}' $docker_name:$name 2> /dev/null | cut -d: -f2 \
                 || echo >&2 WARN: docker digest missing`
