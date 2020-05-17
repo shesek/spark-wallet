shesek/spark-wallet

commit a28002e37b1b1ae98b8881e0154fbd60d14aa412

Update npm dependencies

src/auth.js
@@ -3,8 +3,7 @@ import path from 'path'
 import assert from 'assert'
 import mkdirp from 'mkdirp'
 import basicAuth from 'basic-auth'
-import nanoid from 'nanoid'
-import nanogen from 'nanoid/generate'
+import { nanoid, customAlphabet } from 'nanoid'
 import cookieParser from 'cookie-parser'
 import {createHmac} from 'crypto'
 
@@ -25,7 +24,7 @@ module.exports = (app, cookieFile, login) => {
     ;[ username, password, accessKey ] = fs.readFileSync(cookieFile).toString('utf-8').trim().split(':')
     assert(password, `Invalid login file at ${cookieFile}, expecting "username:pwd[:access-key]"`)
   } else { // generate random
-    username = nanogen('abcdefghijklmnopqrstuvwxyz', 5)
+    username = customAlphabet('abcdefghijklmnopqrstuvwxyz', 5)
     password = nanoid(15)
     accessKey = hmacStr(`${username}:${password}`, 'access-key')
     console.log(`No LOGIN or --login specified, picked username "${username}" with password "${password}"`)

client/src/intent.js
@@ -1,6 +1,6 @@
 import { Observable as O } from './rxjs'
 import serialize from 'form-serialize'
-import nanoid from 'nanoid'
+import { nanoid } from 'nanoid'
 import { dbg, parseUri, parseRpcCmd } from './util'
 
 module.exports = ({ DOM, route, conf$, scan$, urihandler$ }) => {
@@ -49,7 +49,7 @@ module.exports = ({ DOM, route, conf$, scan$, urihandler$ }) => {
   , togExp$   = click('.toggle-exp')
 
   // Dismiss alert message
-  , dismiss$ = O.merge(submit('form'), click('[data-dismiss=alert], a.navbar-brand, .content a, .content button')
+  , dismiss$ = O.merge(click('[data-dismiss=alert], a.navbar-brand, .content a, .content button')
                      , page$.filter(p => p.search != '?r'))
 
   // Payments feed page navigation and click-to-toggle

commit 4fd8f9fd50eecf1b341fbbafe0d802182ba1cb17

Fix bug in automatic username generation (#145)
This was a regression introduced in a28002e37b1b1ae98b8881e0154fbd60d14aa412
when updating nanoid from v2 to v3, due to a change in their public API.

src/auth.js
@@ -24,7 +24,7 @@ module.exports = (app, cookieFile, login) => {
     ;[ username, password, accessKey ] = fs.readFileSync(cookieFile).toString('utf-8').trim().split(':')
     assert(password, `Invalid login file at ${cookieFile}, expecting "username:pwd[:access-key]"`)
   } else { // generate random
-    username = customAlphabet('abcdefghijklmnopqrstuvwxyz', 5)
+    username = customAlphabet('abcdefghijklmnopqrstuvwxyz', 5)()
     password = nanoid(15)
     accessKey = hmacStr(`${username}:${password}`, 'access-key')
     console.log(`No LOGIN or --login specified, picked username "${username}" with password "${password}"`)