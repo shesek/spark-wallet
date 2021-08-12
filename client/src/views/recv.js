54631912b2b32290094d0908f5f6c243cc6ab2eb
Nadav Ivgi <nadav@shesek.info>
2021-08-12 11:10:22 +0300

    ui: Improve "Receive payment" screen

--- client/src/views/recv.js
+++ client/src/views/recv.js
@@ -1,9 +1,13 @@
 import { div, form, input, button, a, span, p, img, h2, h3, small } from '@cycle/dom'
-import { formGroup, yaml, qrinv, amountField, omitKey, fancyCheckbox } from './util'
+import { formGroup, yaml, qrinv, amountField, omitKey, fancyCheckbox, fmtSatAmountWithAlt } from './util'
 
 const recv = ({ amtData, offersEnabled, invUseOffer }) =>
-  form({ attrs: { do: 'new-invoice' } }, [
-    h2('Request payment')
+  // Wait for 'listconfigs' to tell us if offers support is enabled,
+  // to prevent the UI from jumping around.
+  offersEnabled == null ? div('.loader.inline')
+
+: form({ attrs: { do: 'new-invoice' } }, [
+    h2('Receive payment')
   , formGroup('Payment amount', amountField(amtData, 'msatoshi', false))
 
   , formGroup('Description'
@@ -13,18 +17,20 @@ const recv = ({ amtData, offersEnabled, invUseOffer }) =>
   , offersEnabled ? fancyCheckbox('reusable-offer', 'Reusable offer (BOLT 12)', invUseOffer) : ''
 
   , div('.form-buttons', [
-      button('.btn.btn-lg.btn-primary', { attrs: { type: 'submit' } }, 'Request')
+      button('.btn.btn-lg.btn-primary', { attrs: { type: 'submit' } }, 'Continue')
     , ' '
     , a('.btn.btn-lg.btn-secondary', { attrs: { href: '#/' } }, 'Cancel')
     ])
+
+  , offersEnabled ? p(a('.small.text-muted', { attrs: { href: '#/scan' } }, 'Receive with a withdrawal offer »')) : ''
   ])
 
 const invoice = inv => qrinv(inv).then(qr => ({ unitf, conf: { expert } }) =>
   div('.waiting-payment', [
     div('.row', [
       div('.col-sm-6.text-center', [
-        h2('Waiting for payment')
-      , inv.msatoshi !== 'any' ? h3('.toggle-unit', unitf(inv.msatoshi)) : ''
+        h2('Receive payment')
+      , inv.msatoshi !== 'any' ? h3('.toggle-unit', fmtSatAmountWithAlt(inv.msatoshi, unitf)) : ''
       , small('.d-none.d-sm-block.text-muted.break-all.mt-3', inv.bolt11)
       ])
     , div('.col-sm-6.text-center', [
